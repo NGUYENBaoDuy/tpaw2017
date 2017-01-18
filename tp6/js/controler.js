f1271b9c20e89927e5e5bc1655d3549e60925324
NGUYENBaoDuy <baoduy2511@gmail.com>
2017-01-18 21:40:33 +0100

    Add files via upload

--- tp6/js/controler.js
+++ tp6/js/controler.js
@@ -18,7 +18,7 @@ meteoControllers.controller('MainController', ['$scope', '$http',
         $scope.GPS = function () {
             navigator.geolocation.getCurrentPosition(function (position) {
                 /* appel AJAX à l’API openweathermap */
-                $http.get('http://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&units=metric&appid=ae3e8be45d47ccd5941b17ff8f3aba94')
+                $http.get('https://api.openweathermap.org/data/2.5/weather?lat=' + position.coords.latitude + '&lon=' + position.coords.longitude + '&units=metric&appid=ae3e8be45d47ccd5941b17ff8f3aba94')
                     .success(function (data) {
                         /* on met dans l’objet ​meteo les données retournées
                          par openweathermap */
